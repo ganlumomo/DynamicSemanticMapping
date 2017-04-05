0ac4b55221dda04c9073ce1d0452c941695dd2fb
Yiqian Gan <ganyq@umich.edu>
2017-04-05 17:53:39 -0400

    add all the procedure

--- octomap/octomap/src/testing/test_semantic_prediction.cpp
+++ octomap/octomap/src/testing/test_semantic_prediction.cpp
@@ -135,31 +135,43 @@ int main(int argc, char** argv) {
     std::cout << label << std::endl;  
     
     point3d new_pos;
-//    float new_pos[3];
     for (int j=0;j<3;j++){
-      new_pos(j) = query(j) + sceneflow(j) + 0*error(j);
-      cout << "new_pos " << new_pos(j) << "  query  " << query(j) << "\n" << endl;
+      new_pos(j) = query(j) + sceneflow(j) + error(j);
+      // cout << "new_pos " << new_pos(j) << "  query  " << query(j) << "\n" << endl;
     }
     
-//      pose6d origin(0, 0, 0, 0, 0, 0);
-////    Pointcloud* cloud = new Pointcloud();
-//      temp_tree.insertPointCloud(*new_cloud, origin.trans());
-////      
-//      // fuse extra information
-////      for (int i=0; i < (int)new_cloud->size(); ++i) {
-//        //        //std::vector<float> extra_info = cloud->getExtraInfo(i);
-        
+
         float ol = 10.0;
         SemanticOcTreeNode* newNode = tree.setNodeValue(new_pos, ol);
+        SemanticOcTreeNode* newNode1 = tree.setNodeValue(new_pos, ol+1);
+        cout<<"if node are comparable->"<<(newNode==newNode1)<<endl;
+        // cout<<"positions"<<newNode->getCoordinate () <<endl;
         newNode->setSemantics(s);
         //tree.averageNodeSemantics(newNode, label);
 //        //print_query_info(query, n);  
   }
     
+//find the weights/number of particles
+
+
+
+//second loop for all the points, propagate using sceneflow
+
+// store in temp tree. Delete voxels thos 
+
+
+//normalize
+
+
+// smoothing
+ 
+
+//update the original tree
+
+
 //	
 //FOR SANITY 
   
-
   // traverse the whole tree, set color based on semantics to visualize
   for (SemanticOcTree::iterator it = tree.begin(); it != tree.end(); ++it) {
     if ( (&(*it))->isSemanticsSet() ) {
@@ -177,23 +189,7 @@ int main(int argc, char** argv) {
 
   tree.write("semantic_color_scan_Moved.ot");
 
-//
-//  // traverse the whole tree, set color based on semantics to visualize
-//  for (SemanticOcTree::iterator it = temp_tree.begin(); it != temp_tree.end(); ++it) {
-//    if ( (&(*it))->isSemanticsSet() ) {
-//      SemanticOcTreeNode::Semantics s = (&(*it))->getSemantics();
-//      // Debug
-//      //print_query_info(point3d(0,0,0), &(*it));  
-//      for (int argn = 0; argn < argc-1; argn++) {
-//        if (s.label.size() && s.label[argn] > 0.3) {
-//          (&(*it))->setColor(color[argn][0], color[argn][1], color[argn][2]);
-//        }
-//      }
-//    }
-//  }//end for
-//
-//
-//  temp_tree.write("semantic_color_scan_moved.ot");
+
 
   
 
